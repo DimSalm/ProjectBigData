7361ebb831d384cf2bcb2cd6da86a96959a535ba
Dimitris Salmatanis <D.salmatanis@acg.edu>
2020-11-23 17:26:56 +0200

    Initial commit

--- projectQ1.py
+++ projectQ1.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import csv
 
-books = pd.read_csv('BX-Books_clean.csv',delimiter=';',encoding='Latin-1',dtype={3:'string'})
+books = pd.read_csv('BX-Books_clean.csv',delimiter=';',encoding='Latin-1',dtype={3:'object'})
 ratings = pd.read_csv('BX-Book-Ratings.csv',delimiter=';',encoding='Latin-1')
 users = pd.read_csv('BX-Users.csv',delimiter=';',encoding='Latin-1')
-print(books.head())
-print(ratings.head())
-print(users.head())
+print('books rows,columns and info: ',books.shape)
+print(books.info(memory_usage='deep'))
+print('book ratings rows,columns and info: ',ratings.shape)
+print(ratings.info(memory_usage='deep'))
+print('users rows,columns and info: ',users.shape)
+print(users.info(memory_usage='deep'))
