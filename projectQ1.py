1eb87e430ff18b271e67ad09a5d61362e01c9aea
Dimitris Salmatanis <D.salmatanis@acg.edu>
2020-11-25 11:13:17 +0200

    updated q1

--- projectQ1.py
+++ projectQ1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import csv
 
+#Q1 read-shape-info and do we nned info from read me file as comments?edw mporoume na to grapsoume kalitera
 books = pd.read_csv('BX-Books_clean.csv',delimiter=';',encoding='Latin-1',dtype={3:'object'})
 ratings = pd.read_csv('BX-Book-Ratings.csv',delimiter=';',encoding='Latin-1')
 users = pd.read_csv('BX-Users.csv',delimiter=';',encoding='Latin-1')
@@ -11,3 +11,22 @@ print('book ratings rows,columns and info: ',ratings.shape)
 print(ratings.info(memory_usage='deep'))
 print('users rows,columns and info: ',users.shape)
 print(users.info(memory_usage='deep'))
+
+br = pd.merge(books,ratings,how='outer')
+bru = pd.merge(br,users,how='outer')
+print(bru.info())
+
+#book popularity = what book have most ratings (means people have read it)
+bookpop = bru.groupby(['ISBN'])[['Book-Rating']].count()
+print(bookpop.sort_values(['Book-Rating'],ascending=False))
+
+#author popularity = group all the books of an author and count how many have read all of them
+authorpop = bru.groupby(['Book-Author'])[['Book-Rating']].count()
+print(authorpop.sort_values(['Book-Rating'],ascending=False))
+
+#How many books each age group have read
+agegroups = pd.cut(bru['Age'], bins=[0, 20, 40, 60, 80, np.inf])
+ageranges = bru.groupby(agegroups)[['ISBN']].count()
+print(ageranges.sort_values(['ISBN'],ascending=False))
+
+#Outlier detection
