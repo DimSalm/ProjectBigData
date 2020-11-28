d173a048c0d4c5bc76ab51840c1b23c61b6db096
Dimitris Salmatanis <D.salmatanis@acg.edu>
2020-11-28 02:34:51 +0200

    last try

--- Q1UnderstandingTheData.py
+++ Q1UnderstandingTheData.py
@@ -7,8 +7,11 @@ ratings = pd.read_csv('BX-Book-Ratings.csv',delimiter=';',encoding='Latin-1')
 users = pd.read_csv('BX-Users.csv',delimiter=';',encoding='Latin-1')
 
 print(books.info(memory_usage='deep'))
+print('books rows,columns and info: ',books.shape)
 print(ratings.info(memory_usage='deep'))
+print('book ratings rows,columns and info: ',ratings.shape)
 print(users.info(memory_usage='deep'))
+print('users rows,columns and info: ',users.shape)
 
 #clean books:year of puplication
 booksclean1 = books[books['Year-Of-Publication']<=2020]
@@ -34,7 +37,7 @@ print(usersclean2)
 
 #keep only the commons
 ratings_clean1 = ratingsclean1[ratingsclean1['ISBN'].isin(booksclean3['ISBN'])]
-users_clean = usersclean2[usersclean2['User-ID'].isin(ratingsclean1['User-ID'])]
+users_clean = usersclean2[usersclean2['User-ID'].isin(ratings_clean1['User-ID'])]
 print(users_clean)
 
 ratings_clean = ratings_clean1[ratings_clean1['User-ID'].isin(users_clean['User-ID'])]
@@ -43,17 +46,34 @@ print(ratings_clean)
 books_clean = booksclean3[booksclean3['ISBN'].isin(ratings_clean['ISBN'])]
 print(books_clean)
 
-#print(usersclean1.isna().sum())
-#print(usersclean2.groupby('Age')['Age'].value_counts())
+#book popularity = what book have most ratings (means people have read it)
+bookpop = ratings_clean.groupby(['ISBN'])[['Book-Rating']].count().sort_values(['Book-Rating'],ascending=False)
+print(bookpop)
+
+#author popularity = group all the books of an author and count how many have read all of them
+bru1 = pd.merge(books_clean,ratings_clean)
+authorpop = bru1.groupby(['Book-Author'])[['Book-Rating']].count().sort_values(['Book-Rating'],ascending=False)
+print(authorpop)
+
+#How many books each age group have read
+bru2 = pd.merge(users_clean,ratings_clean)
+agegroups = pd.cut(bru2['Age'], bins=[14, 20, 40, 60, 80,90])
+ageranges = bru2.groupby(agegroups)[['Book-Rating']].count().sort_values(['Book-Rating'],ascending=False)
+print(ageranges)
+
+#rating Outlier detection where rating=0 and where number of times the book was read is 1
+booksread = ratings_clean[~(ratings_clean['Book-Rating']==0)]
+print(booksread)
+booksread2=booksread.groupby(['ISBN']).count()
+booksread2_clean=booksread[booksread2['Book-Rating']>1]
+print(booksread2_clean)
+poutsaki=booksread.merge(booksread2_clean,left_on=['ISBN'],right_on='ISBN',how='right')
+print(poutsaki)
 
 
-
-
-
-
-
-
-
+#book pop outlier detection based on author
+#authorread = books_clean[books_clean['Book-Author'].isin(authorpop['Book-Author']==1)]
+#print(authorread)
 
 
 
